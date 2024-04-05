78fc5b3f218d812b27dddb333054eae899f116fc
SmitRami1 <122784110+SmitRami1@users.noreply.github.com>
2024-04-05 15:34:37 +0530

    Add files via upload

--- elec-era.sql
+++ elec-era.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 03, 2024 at 06:48 AM
--- Server version: 8.2.0
--- PHP Version: 8.2.13
+-- Generation Time: Apr 05, 2024 at 10:03 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `name` varchar(100) NOT NULL,
+  `image` varchar(1024) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`name`, `image`) VALUES
+('Laptop', 'https://tech.co.za/wp-content/uploads/2022/11/Hp-Probook-450-G9a.png'),
+('Fridge', 'https://www.westinghouse.com.au/remote.jpg.ashx?urlb64=aHR0cHM6Ly9yZXNvdXJjZS5lbGVjdHJvbHV4LmNvbS5hdS9wdWJsaWMvaW1hZ2UyL3Byb2R1Y3QvMTEyNDMzLzU2NTE4L1dILVByb2R1Y3Rab29tL2ZyaWRnZXNfd3FlNTY1MGJhX2dhbGxlcnlfMS5wbmc&hmac=Lrz185oZey4'),
+('T.V', 'https://www.intex.in/cdn/shop/products/3265_1024x1024.png?v=1648711141');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -35,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(100) NOT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
@@ -43,7 +64,10 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`name`, `email`, `pincode`, `password`, `id`) VALUES
 ('Smit', 'smit@email.com', 121212, '1212', 1),
-('dsd', 'smi1t@email.com', 121212, '1212', 2);
+('dsd', 'smi1t@email.com', 121212, '1212', 2),
+('Smityoo', 'smitrami@123.com', 121212, 'Smit', 3),
+('Hrishabh', 'hrishabh@email.com', 121212, '123', 6),
+('Rastogi', 'hrishabh1@email.com', 121232, '1234', 8);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
